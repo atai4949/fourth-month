651615c2b9474957b076d36536a6b98b9797dc2a
atai4949 <108147365+atai4949@users.noreply.github.com>
2022-08-06 17:21:59 +0600

    Add files via upload

--- src/pages/contactsPage/contactsPage.js
+++ src/pages/contactsPage/contactsPage.js
@@ -1,1 +1,14 @@
+import React from "react";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+
+export default function contactsPage () {
+    return (
+        <div>
+            <Header/>
+            <h1> Contact </h1>
+            <Footer/>
+        </div>
+    )
+}
 
